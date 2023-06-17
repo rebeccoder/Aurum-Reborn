354eee238a95bdad9115fbdeea33502122334307
rebeccoder <rebsoptay@icloud.com>
2023-06-17 14:12:31 +0000

    fix corrupted database, add products_images media folder and removes product_images fixtures for now

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import Product, Category, Creator
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['name', 'category', 'price', 'karat', 'creator']
+    list_filter = ['category', 'karat', 'creator']
+    search_fields = ['name']
 
-# Register your models here.
+    fieldsets = (
+        ('Product Information', {
+            'fields': ('sku', 'category', 'name', 'friendly_name', 'description', 'price', 'karat', 'creator')
+        }),
+        ('Image', {
+            'fields': ('image',),
+        }),
+    )
--- products/models.py
+++ products/models.py
@@ -1,5 +1,26 @@
 from django.db import models
 from creators.models import Creator
 
+from django.db import models
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+    friendly_name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
 class Product(models.Model):
-    creator = models.ForeignKey(Creator, on_delete=models.CASCADE)
+    sku = models.CharField(max_length=100, primary_key=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+    friendly_name = models.CharField(max_length=100)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=8, decimal_places=2)
+    karat = models.CharField(max_length=100)
+    creator = models.ForeignKey('creators.Creator', on_delete=models.CASCADE)
+    image = models.ImageField(upload_to='media/product_images/', default='media/image.jpg')
+
+    def __str__(self):
+        return self.name
