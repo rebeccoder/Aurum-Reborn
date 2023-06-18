50bfcbb420ecc1335a1ec404422595d0eac01343
rebeccoder <rebsoptay@icloud.com>
2023-06-18 13:20:42 +0000

    adds more sorting functionality

--- products/views.py
+++ products/views.py
@@ -9,23 +9,23 @@ def all_products(request):
     products = Product.objects.all()
     query = None
     categories = None
+    sort = None
+    direction = None
 
     if request.GET:
         if 'category' in request.GET:
-            categories = request.GET['category'].split(',')
+            categories = request.GET.getlist('category')
             products = products.filter(category__name__in=categories)
             categories = Category.objects.filter(name__in=categories)
-            sort = None
-        direction = None
 
-    if request.GET:
         if 'sort' in request.GET:
             sortkey = request.GET['sort']
             sort = sortkey
-            if sortkey == 'friendly_name':
+            if sortkey == 'name':
                 sortkey = 'lower_name'
-                products = products.annotate(lower_name=Lower('friendly_name'))
-
+                products = products.annotate(lower_name=Lower('name'))
+            if sortkey == 'category':
+                sortkey = 'category__name'
             if 'direction' in request.GET:
                 direction = request.GET['direction']
                 if direction == 'desc':
