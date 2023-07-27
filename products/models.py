c8de5398fef0affe321f85fbd0ef1ede484ea7b2
rebeccoder <rebsoptay@icloud.com>
2023-07-27 17:25:36 +0000

    created the add product view, templaate and url

--- products/models.py
+++ products/models.py
@@ -4,11 +4,18 @@ from creators.models import Creator
 from django.db import models
 
 class Category(models.Model):
-    name = models.CharField(max_length=100)
-    friendly_name = models.CharField(max_length=100)
+
+    class Meta:
+        verbose_name_plural = 'Categories'
+        
+    name = models.CharField(max_length=254)
+    friendly_name = models.CharField(max_length=254, null=True, blank=True)
 
     def __str__(self):
         return self.name
+
+    def get_friendly_name(self):
+        return self.friendly_name
 
 
 class Product(models.Model):
--- products/views.py
+++ products/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
 from django.db.models import Q
+
 from .models import Product, Category
+from .forms import ProductForm
 
 def all_products(request):
     """ A view to show all products, including sorting and search queries """
@@ -59,3 +61,13 @@ def product_detail(request, sku):
         'product': product,
     }
     return render(request, 'products/product_detail.html', context)
+
+def add_product(request):
+    """ Add a product to the store """
+    form = ProductForm()
+    template = 'products/add_product.html'
+    context = {
+        'form': form,
+    }
+
+    return render(request, template, context)
