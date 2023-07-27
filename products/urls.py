26d263559ad0a03cb672c6a7d30077fa37fe1cf4
rebeccoder <rebsoptay@icloud.com>
2023-07-27 21:58:22 +0000

    created edited functionality for product management

--- products/urls.py
+++ products/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('', views.all_products, name='products'),
     path('add/', views.add_product, name='add_product'),
     path('<str:sku>/', views.product_detail, name='product_detail'),
+    path('edit/<str:product_sku>/', views.edit_product, name='edit_product'),
 ]
--- products/views.py
+++ products/views.py
@@ -81,3 +81,27 @@ def add_product(request):
     }
 
     return render(request, template, context)
+
+
+def edit_product(request, product_sku):
+    """ Edit a product in the store """
+    product = get_object_or_404(Product, pk=product_sku)
+    if request.method == 'POST':
+        form = ProductForm(request.POST, request.FILES, instance=product)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Successfully updated product!')
+            return redirect(reverse('product_detail', args=[product.sku]))
+        else:
+            messages.error(request, 'Failed to update product. Please ensure the form is valid.')
+    else:
+        form = ProductForm(instance=product)
+        messages.info(request, f'You are editing {product.friendly_name}')
+
+    template = 'products/edit_product.html'
+    context = {
+        'form': form,
+        'product': product,
+    }
+
+    return render(request, template, context)
