e26390397cf16d49209275e07151252c7ab15129
rebeccoder <rebsoptay@icloud.com>
2023-07-27 16:19:27 +0000

    adds initial notification css, shopping bag preview and messages

--- bag/views.py
+++ bag/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect, reverse, HttpResponse
+from django.shortcuts import render, redirect, reverse, HttpResponse, get_object_or_404
 from django.contrib import messages
 
 from products.models import Product
@@ -19,6 +19,7 @@ def add_to_bag(request, item_id):
 
     if item_id in list(bag.keys()):
         bag[item_id] += quantity
+        messages.success(request, f'Updated {product.friendly_name} quantity to {bag[item_id]}')
     else:
         bag[item_id] = quantity
         messages.success(request, f'Added {product.friendly_name} to your bag')  # This sets the success message.
@@ -30,13 +31,16 @@ def add_to_bag(request, item_id):
 def adjust_bag(request, item_id):
     """Adjust the quantity of the specified product to the specified amount"""
 
+    product = get_object_or_404(Product, pk=item_id)
     quantity = int(request.POST.get('quantity'))
     bag = request.session.get('bag', {})
 
     if quantity > 0:
         bag[item_id] = quantity
+        messages.success(request, f'Updated {product.friendly_name} quantity to {bag[item_id]}')
     else:
         bag.pop(item_id, None)
+        messages.success(request, f'Removed {product.friendly_name} from your bag')
 
     request.session['bag'] = bag
     return redirect(reverse('view_bag'))
@@ -45,11 +49,15 @@ def adjust_bag(request, item_id):
 def remove_from_bag(request, item_id):
     """Remove the item from the shopping bag"""
 
+    product = get_object_or_404(Product, pk=item_id)
     try:
         bag = request.session.get('bag', {})
         bag.pop(item_id, None)
+        messages.success(request, f'Removed {product.friendly_name} from your bag')
+
         request.session['bag'] = bag
         return HttpResponse(status=200)
 
     except Exception as e:
+        messages.error(request, f'Error removing item: {e}')
         return HttpResponse(status=500)
