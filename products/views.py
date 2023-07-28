21fa60d97c1c85d232bedb8381e921c557581612
rebeccoder <rebsoptay@icloud.com>
2023-07-28 13:45:11 +0000

    secure the edit and delete views to super users and the profile views to logged in user

--- products/views.py
+++ products/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 from django.db.models import Q
 
 from .models import Product, Category
@@ -62,8 +63,13 @@ def product_detail(request, sku):
     }
     return render(request, 'products/product_detail.html', context)
 
+@login_required
 def add_product(request):
     """ Add a product to the store """
+    if not request.user.is_superuser:
+        messages.error(request, 'Sorry, only store owners can do that.')
+        return redirect(reverse('home'))
+
     if request.method == 'POST':
         form = ProductForm(request.POST, request.FILES)
         if form.is_valid():
@@ -82,8 +88,13 @@ def add_product(request):
 
     return render(request, template, context)
 
+@login_required
 def edit_product(request, product_sku):
     """ Edit a product in the store """
+    if not request.user.is_superuser:
+        messages.error(request, 'Sorry, only store owners can do that.')
+        return redirect(reverse('home'))
+
     product = get_object_or_404(Product, pk=product_sku)
     if request.method == 'POST':
         form = ProductForm(request.POST, request.FILES, instance=product)
@@ -105,8 +116,13 @@ def edit_product(request, product_sku):
 
     return render(request, template, context)
 
+@login_required
 def delete_product(request, product_sku):
     """ Delete a product from the store """
+    if not request.user.is_superuser:
+        messages.error(request, 'Sorry, only store owners can do that.')
+        return redirect(reverse('home'))
+
     product = get_object_or_404(Product, pk=product_sku)
     product.delete()
     messages.success(request, 'Product deleted!')
