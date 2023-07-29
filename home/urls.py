b6193d281093c094f00a046b8d708f51b677bd39
rebeccoder <rebsoptay@icloud.com>
2023-07-29 17:52:26 +0000

    creates seperate file for mobile homepage

--- home/urls.py
+++ home/urls.py
@@ -1,6 +1,8 @@
-from django.urls import path, include
+from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home')
+    path('', views.home, name='home'),
+    path('mobile-home.html', views.mobile_home, name='mobile_home'),
+    path('mobile_redirect/', views.mobile_redirect, name='mobile_redirect'),
 ]
--- home/views.py
+++ home/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from creators.models import Creator
+from django.http import HttpResponsePermanentRedirect
+
 
 # Create your views here.
 def index(request):
@@ -15,3 +17,16 @@ def home(request):
         'creators': creators,
     }
     return render(request, 'home/index.html', context)
+
+def mobile_home(request):
+    # Your logic to render the mobile home page goes here
+    return render(request, 'home/mobile-home.html')
+
+def mobile_redirect(request):
+    user_agent = request.META['HTTP_USER_AGENT']
+
+    # Check if the user is on a mobile device
+    if 'Mobile' in user_agent:
+        return redirect('mobile_home')
+    else:
+        return redirect('home')
