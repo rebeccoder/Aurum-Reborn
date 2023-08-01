22c26c8fccbe2de166ec9ea479a40430616b3fc2
rebeccoder <rebsoptay@icloud.com>
2023-08-01 04:43:40 +0000

    updated debug settings

--- aurum_reborn/settings.py
+++ aurum_reborn/settings.py
@@ -23,8 +23,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY', '')
+SECRET_KEY = os.environ.get("SECRET_KEY")
+
 # SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = "DEVELOPMENT" in os.environ
+
 ACCOUNT_EMAIL_VERIFICATION = 'none'
 
 ALLOWED_HOSTS = [
@@ -33,7 +36,6 @@ ALLOWED_HOSTS = [
     "aurum-reborn-46fdfe47f435.herokuapp.com"
 ]
 
-DEBUG = True
 
 INSTALLED_APPS = [
     'django.contrib.admin',
