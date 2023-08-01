d37be53faa7e7a7d48a1b3cf8e871bc4df26f2f9
rebeccoder <rebsoptay@icloud.com>
2023-08-01 04:28:52 +0000

    fixing the checkout views

--- checkout/views.py
+++ checkout/views.py
@@ -26,8 +26,7 @@ def cache_checkout_data(request):
         })
         return HttpResponse(status=200)
     except Exception as e:
-        messages.error(request,
-                       'Sorry, your payment cannot be processed right now. Please try again later.')
+        messages.error(request, 'Sorry, your payment cannot be processed right now. Please try again later.')
         return HttpResponse(content=e, status=400)
 
 
@@ -52,83 +51,51 @@ def checkout(request):
 
         order_form = OrderForm(form_data)
 
+        if order_form.is_valid():
+            order = order_form.save(commit=False)
+            pid = request.POST.get('client_secret').split('_secret')[0]
+            order.stripe_pid = pid
+            order.original_bag = json.dumps(bag)
+            order.save()
 
-if order_form.is_valid():
-    order = order_form.save(commit=False)
-    pid = request.POST.get('client_secret').split('_secret')[0]
-    order.stripe_pid = pid
-    order.original_bag = json.dumps(bag)
-    order.save()
+            for item_id, item_data in bag.items():
+                try:
+                    product = Product.objects.get(id=item_id)
+                    if isinstance(item_data, int):
+                        order_line_item = OrderLineItem(
+                            order=order,
+                            product=product,
+                            quantity=item_data,
+                        )
+                        order_line_item.save()
+                    else:
+                        for quantity, size in item_data.items():
+                            order_line_item = OrderLineItem(
+                                order=order,
+                                product=product,
+                                quantity=quantity,
+                                product_size=size,
+                            )
+                            order_line_item.save()
+                except Product.DoesNotExist:
+                    messages.error(request, (
+                        "One of the products in your bag wasn't found in our database. "
+                        "Please call us for assistance!"
+                    ))
+                    order.delete()
+                    return redirect(reverse('view_bag'))
 
-    for item_id, item_data in bag.items():
-        try:
-            product = Product.objects.get(id=item_id)
-            if isinstance(item_data, int):
-                order_line_item = OrderLineItem(
-                    order=order,
-                    product=product,
-                    quantity=item_data,
-                )
-                order_line_item.save()
-            else:
-                for size, quantity in item_data.items():
-                    order_line_item = OrderLineItem(
-                        order=order,
-                        product=product,
-                        quantity=quantity,
-                        product_size=size,
-                    )
-                    order_line_item.save()
-        except Product.DoesNotExist:
-            messages.error(request, (
-                "One of the products in your bag wasn't found in our database. "
-                "Please call us for assistance!"
-            ))
-            order.delete()
-            return redirect(reverse('view_bag'))
+            request.session['save_info'] = 'save-info' in request.POST
+            return redirect(reverse('checkout_success', args=[order.order_number]))
+        else:
+            messages.error(request, 'There was an error with your form. Please double-check your information.')
+            return redirect(reverse('checkout'))
 
-request.session['save_info'] = 'save-info' in request.POST
-if order_form.is_valid():
-    order = order_form.save(commit=False)
-    pid = request.POST.get('client_secret').split('_secret')[0]
-    order.stripe_pid = pid
-    order.original_bag = json.dumps(bag)
-    order.save()
-    for item_id, item_data in bag.items():
-        try:
-            product = Product.objects.get(id=item_id)
-            if isinstance(item_data, int):
-                order_line_item = OrderLineItem(
-                    order=order,
-                    product=product,
-                    quantity=item_data,
-                )
-                order_line_item.save()
-            else:
-                for quantity, size in item_data.items():
-                    order_line_item = OrderLineItem(
-                        order=order,
-                        product=product,
-                        quantity=quantity,
-                        product_size=size,
-                    )
-                    order_line_item.save()
-        except Product.DoesNotExist:
-            messages.error(request, (
-                "One of the products in your bag wasn't found in our database. "
-                "Please call us for assistance!"
-            ))
-            order.delete()
-            return redirect(reverse('view_bag'))
-    request.session['save_info'] = 'save-info' in request.POST
-    return redirect(reverse('checkout_success', args=[order.order_number]))
-else:
-    messages.error(request, 'There was an error with your form. Please double-check your information.')
-else:
-    bag = request.session.get('bag', {})
-    if not bag:
-        messages.error(request, "There's nothing in your bag at the moment")
-        return redirect(reverse('products'))
+    else:
+        bag = request.session.get('bag', {})
+        if not bag:
+            messages.error(request, "There's nothing in your bag at the moment")
+            return redirect(reverse('products'))
 
         current_bag = bag_contents(request)
         total = current_bag['grand_total']
@@ -159,33 +126,17 @@ else:
         else:
             order_form = OrderForm()
 
-    if not stripe_public_key:
-        messages.warning(request, 'Stripe public key is missing. \
-            Did you forget to set it in your environment?')
+        if not stripe_public_key:
+            messages.warning(request, 'Stripe public key is missing. Did you forget to set it in your environment?')
 
-    template = 'checkout/checkout.html'
-    context = {
-        'order_form': order_form,
-        'stripe_public_key': stripe_public_key,
-        'client_secret': intent.client_secret,
-    }
+        template = 'checkout/checkout.html'
+        context = {
+            'order_form': order_form,
+            'stripe_public_key': stripe_public_key,
+            'client_secret': intent.client_secret,
+        }
 
-    return render(request, template, context)
-    bag = request.session.get('bag', {})
-
-    bag = request.session.get('bag', {})
-
-if not stripe_public_key:
-    messages.warning(request, 'Stripe public key is missing. Did you forget to set it in your environment?')
-
-template = 'checkout/checkout.html'
-context = {
-    'order_form': order_form,
-    'stripe_public_key': stripe_public_key,
-    'client_secret': intent.client_secret,
-}
-
-return render(request, template, context)
+        return render(request, template, context)
 
 
 def checkout_success(request, order_number):
@@ -214,49 +165,6 @@ def checkout_success(request, order_number):
                 user_profile_form.save()
 
     messages.success(request, f'Order successfully processed! Your order number is {order_number}. A confirmation email will be sent to {order.email}.')
-
-    if 'bag' in request.session:
-        del request.session['bag']
-
-    template = 'checkout/checkout_success.html'
-    context = {
-        'order': order,
-    }
-
-    return render(request, template, context)
-
-
-def checkout_success(request, order_number):
-    """
-    Handle successful checkouts
-    """
-    save_info = request.session.get('save_info')
-    order = get_object_or_404(Order, order_number=order_number)
-
-    if request.user.is_authenticated:
-        profile = UserProfile.objects.get(user=request.user)
-        # Attach the user's profile to the order
-        order.user_profile = profile
-        order.save()
-
-        # Save the user's info
-        if save_info:
-            profile_data = {
-                'default_phone_number': order.phone_number,
-                'default_country': order.country,
-                'default_postcode': order.postcode,
-                'default_town_or_city': order.town_or_city,
-                'default_street_address1': order.street_address1,
-                'default_street_address2': order.street_address2,
-                'default_county': order.county,
-            }
-            user_profile_form = UserProfileForm(profile_data, instance=profile)
-            if user_profile_form.is_valid():
-                user_profile_form.save()
-
-    messages.success(request, f'Order successfully processed! \
-        Your order number is {order_number}. A confirmation \
-        email will be sent to {order.email}.')
 
     if 'bag' in request.session:
         del request.session['bag']
