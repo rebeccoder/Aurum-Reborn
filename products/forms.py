5711c238d83de0ef9ebf7656f9144c8d1414b977
rebeccoder <rebsoptay@icloud.com>
2023-09-21 09:46:25 +0000

    changes sku option to read only in editing form

--- products/forms.py
+++ products/forms.py
@@ -4,13 +4,11 @@ from .models import Product, Category
 
 
 class ProductForm(forms.ModelForm):
-
     class Meta:
         model = Product
         fields = '__all__'
 
-    image = forms.ImageField(label='Image',
-                             required=False, widget=CustomClearableFileInput)
+    image = forms.ImageField(label='Image', required=False, widget=CustomClearableFileInput)
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
@@ -20,3 +18,7 @@ class ProductForm(forms.ModelForm):
         self.fields['category'].choices = friendly_names
         for field_name, field in self.fields.items():
             field.widget.attrs['class'] = 'border-black rounded-0'
+
+        if self.instance.pk:
+            self.fields['sku'].widget.attrs['readonly'] = True
+
